e16a407e695f28a4094dd2a191aaa2a9317ad8b9
Thiago Bezerra de Carvalho <thiagaonaruto@gmail.com>
2022-10-21 21:58:56 -0300

    treta demais

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("---bhaskara---");
+double a, b, c, delta, x1, x2;
+
+Console.Write("\ndigite o primeiro algarismo (a):");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\ndigite o segundo algarismo (b):");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\ndigite o terceiro algarismo (c):");
+c = Convert.ToDouble(Console.ReadLine());
+
+if (a == 0)
+{
+    Console.WriteLine("isso não é uma equação de segundo grau...");
+}
+else
+    {}
+    delta = b * b - 4 * a * c;
+    
+    if (delta < 0)
+    {
+        Console.WriteLine($"Como delta = {delta:N2}, a equação não possui raízes reais!");
+    }
+    else
+    {
+        x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+        x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+        Console.WriteLine($"x1 = {x1:N2} e x2 = {x2:N2}");
+    }
+    
